521208118c350bfe4409d045a82951ccefe089a4
Samuel Kiplagat <srklagat@gmail.com>
2022-10-19 17:19:14 +0300

    initial commit

---
+++ src/main/kotlin/srklagat/security/hashing/HashingService.kt
@@ -0,0 +1,6 @@
+package srklagat.security.hashing
+
+interface  HashingService {
+    fun generateSaltedHash(value: String,saltLength : Int = 32) :String
+    fun verify(value: String,saltedHash: SaltedHash):Boolean
+}
---
+++ src/main/kotlin/srklagat/security/hashing/SaltedHash.kt
@@ -0,0 +1,6 @@
+package srklagat.security.hashing
+
+data class SaltedHash(
+    val hash: String,
+    val salt: String
+)
